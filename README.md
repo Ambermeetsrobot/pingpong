0816c734fbab2b065b5f82f02a56ed4ddea33e84
Ambermeetsrobot <amber@empiricalux.com>
2016-03-11 23:55:11 -0800

    try to get answer to appear in UI without success

---
+++ README.md
@@ -0,0 +1,37 @@
+#Ping-pong
+-------------------------------------------------------------------
+##Author:
+Amber Ward
+
+##Project:
+Epicodus Javascript Business Logic Code Review 3.
+
+##Program Name:
+Epicodus Intro to Programming Evening
+
+##Description:
+Users enter a number that returns "ping" if it's divisible by 3, "pong" if it's divisible by 5, and "ping-pong" if it's divisible by 15.
+
+* The site uses HTML, CSS, JavaScript and jQuery
+* Code separates business logic and user interface logic
+* Variable names used are descriptive and user lower camel case
+* The web page is styled using Bootstrap and custom CSS
+
+##Program Set-up instructions:
+
+Click on the Github link provided below. If you are not immediately directed to the page, click on the index.html file in the project folder.
+
+(https://github.com/Ambermeetsrobot/pingpong.git)
+
+##Technologies used
+
+* HTML
+* CSS
+* Bootstrap
+* jQuery
+* Javascript
+* Github
+* Command line
+
+##Copyright Information
+Copyright (c) 2016 Amber M Ward
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
+// var pingpong = function(number) {
+//   var text = "";
+//   for (var i = 0; i < number; i++) {
+//     if (i % 15 === 0) {
+//       text += "pingpong";
+//     } else if (i % 5 === 0) {
+//       text += "pong";
+//     } else if (i % 3 === 0) {
+//       text += "ping";
+//     } else {
+//       text += i;
+//     }
+//   }
+// };
+// I couldn't get this loop version to work in the console
+
 var pingpong = function(number) {
-  if (number % 15 === 0) {
-    return "pingpong";
-  } else if (number % 5 === 0) {
-    return "pong";
-  } else if (number % 3 === 0) {
-    return "ping";
-  } else {
-    return number;
-  }
+    if (number % 15 === 0) {
+      return "ping-pong";
+    } else if (number % 5 === 0) {
+      return "pong";
+    } else if (number % 3 === 0) {
+      return "ping";
+    } else {
+      return number;
+    }
 };
 
-$(document).ready(function(){
+// Not sure why this isn't working in the UI. I've tried .html, .append, .text, and many html variations. Tried debugging, console logs, and alerts. No love.
+$(document).ready(function() {
  $("form#pingpong").submit(function(event) {
- var number = parseInt($("input#number").val());
- var answer = pingpong(number);
- $("ul").append(answer);
+   var number = parseInt($("input#number").val());
+   var answer = pingpong(number);
 
+   $(".answer").append(answer);
+   $("#result").show();
 
- event.preventDefault();
+   event.preventDefault();
 
   });
 });
